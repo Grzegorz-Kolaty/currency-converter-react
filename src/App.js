e63dd6f9e179a09fc0541b662db39c271a0bc6c8
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-16 22:25:45 +0200

    calculating engine added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import Footer from "./Footer";
 import Outcome from "./Outcome";
 import Form from "./Form";
 
-
 function App() {
+  const currencies = [
+    {
+      id: 1,
+      name: "PLN",
+      value: 1
+    },
+    {
+      id: 2,
+      name: "EUR",
+      value: 4
+    },
+    {
+      id: 3,
+      name: "USD",
+      value: 3
+    }
+  ]
   return (
     <Container>
       <Header />
-      <Form />
+      <Form
+        currencies={currencies} />
       <Outcome />
       <Footer />
     </Container>
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,51 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => {
-  // const [currencyInput, onSelectCurrencyInput] = useState("Złoty");
-  // const [currencyOutput, onSelectCurrencyOutput] = useState("Euro");
-  // const [amount, setAmount] = useState("");
+const Form = ({ currencies }) => {
+  const [addAmount, setAmount] = useState("10");
+  const [currencyIn, currencyInValue] = useState("1");
+  const [currencyOut, currencyOutValue] = useState("1");
 
-  // const onFormSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log(event);
-  // };
+  const result = addAmount * currencyIn / currencyOut;
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(result);
+  }
 
   return (
-    <form className="js-form">
+    <form className="form" onSubmit={onFormSubmit}>
       <fieldset className="form__fieldset">
         <legend className="form__legend">Currency calculator</legend>
         <label className="form__label">
           Amount
-          <input className="form__input" name="form" type="number" min="1" step="any" placeholder="1"
-            required />
+          <input
+            value={addAmount}
+            onChange={({ target }) => setAmount(target.value)}
+            className="form__input"
+            name="form"
+            type="number"
+            min="1"
+            step="any"
+            placeholder="100"
+            required
+            autoFocus />
         </label>
         <label className="form__label">
           Currency
-          <select className="form__select" type="select" required>
-            <option value="1.00">PLN</option>
-            <option value="4.82">EUR</option>
-            <option value="4.96">USD</option>
+          <select
+            className="form__select"
+            value={currencyIn}
+            onChange={({ target }) => currencyInValue(target.value)}
+            required
+          >
+            {currencies.map((currency) => (
+              <option
+                key={currency.id}
+                value={currency.value}>
+                {currency.name}
+              </option>
+            ))}
           </select>
         </label>
         <label className="form__label">
@@ -34,13 +54,22 @@ const Form = () => {
         </label>
         <label className="form__label">
           Exchange
-          <select className="form__select" type="select"
-            required>
-            <option value="1.00">PLN</option>
-            <option value="4.82">EUR</option>
-            <option value="4.96">USD</option>
+          <select
+            className="form__select"
+            value={currencyOut}
+            onChange={({ target }) => currencyOutValue(target.value)}
+            required
+          >
+            {currencies.map((currency) => (
+              <option
+                key={currency.id}
+                value={currency.value}>
+                {currency.name}
+              </option>
+            ))}
           </select>
         </label>
+        <button>Calculate</button>
       </fieldset>
     </form>
   )
