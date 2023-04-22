1d556a1a677692e07111cbee7650c89cd96c6f8f
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-22 15:39:58 +0200

    currencies rework, input functions moved to form

--- src/App.js
+++ src/App.js
@@ -6,43 +6,19 @@ import Result from "./Result";
 import Form from "./Form";
 
 function App() {
-  const [result, setResult] = useState("");
-  const [addAmount, setAmount] = useState("10");
-  const [currencyIn, currencyInValue] = useState("1");
-  const [currencyOut, currencyOutValue] = useState("1");
-  console.log(currencyOut);
-
+  const [result, setResult] = useState("10 PLN");
   const currencies = [
-    {
-      id: 1,
-      name: "PLN",
-      value: 1
-    },
-    {
-      id: 2,
-      name: "EUR",
-      value: 4
-    },
-    {
-      id: 3,
-      name: "USD",
-      value: 3
-    }
+    { id: 1, name: "PLN", value: 1 },
+    { id: 2, name: "EUR", value: 4 },
+    { id: 3, name: "USD", value: 3 }
   ];
 
   return (
     <Container>
       <Header title="Currency connverter - just to make things easier!" />
       <Form setResult={setResult}
-        addAmount={addAmount}
-        setAmount={setAmount}
-        currencyIn={currencyIn}
-        currencyInValue={currencyInValue}
-        currencyOut={currencyOut}
-        currencyOutValue={currencyOutValue}
-        currencies={currencies}
-      />
-      <Result title="Exchange value:" result={result} currencyOut={currencyOut} />
+        currencies={currencies} />
+      <Result title="Exchange value:" result={result} />
       <Footer title="Copyright 2023 YouCode Industries" />
     </Container>
   )
