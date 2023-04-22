ce6f4c9c88a130b2fb498acdbb307c315fd7b34a
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-22 15:40:21 +0200

    functions rework, added currency type

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,81 +1,68 @@
+import { useState } from 'react';
 import "./style.css";
 
 const Form =
-  ({ setResult,
-    addAmount,
-    setAmount,
-    currencyIn,
-    currencyInValue,
-    currencyOut,
-    currencyOutValue,
-    currencies }) => {
+  ({ setResult, currencies }) => {
+    const [addAmount, setAmount] = useState("10");
+    const [currencyIn, currencyInValue] = useState("1");
+    const [currencyOut, currencyOutValue] = useState("1");
+    const [selectedCurrency, setSelectedCurrency] = useState("PLN");
 
     const onFormSubmit = (event) => {
       event.preventDefault();
-      const result = addAmount * currencyIn / currencyOut;
-      setResult(result);
+      const result = (addAmount * currencyIn / currencyOut);
+      setResult(`${result} ${selectedCurrency}`);
     };
 
     return (
       <form className="form" onSubmit={onFormSubmit}>
         <fieldset className="form__fieldset">
           <legend className="form__legend">Currency calculator</legend>
-          <label className="form__label">
-            Amount
+          <label className="form__label">Amount
             <input
               value={addAmount}
               onChange={({ target }) => setAmount(target.value)}
               className="form__input"
-              name="form"
               type="number"
               min="1"
-              step="any"
-              placeholder="100"
-              required
               autoFocus />
           </label>
-          <label className="form__label">
-            Currency
+          <label className="form__label">Currency
             <select
               className="form__select"
               value={currencyIn}
-              onChange={({ target }) => currencyInValue(target.value)}
-              required
-            >
+              onChange={({ target }) => {
+                currencyInValue(target.value);
+              }}>
               {currencies.map((currency) => (
-                <option
-                  key={currency.id}
-                  value={currency.value}>
+                <option key={currency.id} value={currency.value}>
                   {currency.name}
                 </option>
               ))}
             </select>
           </label>
-          <label className="form__label">
-            Switch
+          <label className="form__label">Switch
             <p className="form__select">&#8596;</p>
           </label>
-          <label className="form__label">
-            Exchange
+          <label className="form__label">Exchange
             <select
               className="form__select"
               value={currencyOut}
-              onChange={({ target }) => currencyOutValue(target.value)}
-              required
-            >
+              onChange={({ target }) => {
+                currencyOutValue(target.value);
+                setSelectedCurrency(target.options[target.selectedIndex].text);
+              }}>
               {currencies.map((currency) => (
-                <option
-                  key={currency.id}
-                  value={currency.value}>
+                <option key={currency.id} value={currency.value}>
                   {currency.name}
                 </option>
               ))}
             </select>
           </label>
-          <button>Calculate</button>
+          <button className="form__button">Calculate</button>
         </fieldset>
       </form>
-    )
+    );
   };
 
 export default Form;
