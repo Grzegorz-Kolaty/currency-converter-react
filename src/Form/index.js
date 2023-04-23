a1648e7948a0879b37cf8f0d6e18601cca6f9dd4
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-23 17:46:56 +0200

    final css updates

--- src/Form/index.js
+++ src/Form/index.js
@@ -23,49 +23,51 @@ const Form =
       <form className="form" onSubmit={onFormSubmit}>
         <fieldset className="form__fieldset">
           <legend className="form__legend">Currency calculator</legend>
-          <label className="form__label">Amount
-            <input
-              value={addAmount}
-              onChange={({ target }) => setAmount(target.value)}
-              className="form__input"
-              type="number"
-              min="1"
-              step="any"
-              autoFocus />
-          </label>
-          <label className="form__label">Currency
-            <select className="form__select" value={currencyIn}
-              onChange={({ target }) => {
-                currencyInValue(target.value);
-              }}>
-              {currencies.map((currency) => (
-                <option key={currency.id} value={currency.value}>
-                  {currency.name}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label className="form__label">Switch
-            <button
-              onClick={switchCurrencies}
-              className="form__button">
-              &#8596;</button>
-          </label>
-          <label className="form__label">Exchange
-            <select className="form__select" value={currencyOut}
-              onChange={({ target }) => {
-                currencyOutValue(target.value);
-              }}>
-              {currencies.map((currency) => (
-                <option key={currency.id} value={currency.value}>
-                  {currency.name}
-                </option>
-              ))}
-            </select>
-          </label>
-          <button className="form__button">Calculate</button>
+          <div className="form__fieldsetInputs">
+            <label className="form__label">Amount
+              <input
+                value={addAmount}
+                onChange={({ target }) => setAmount(target.value)}
+                className="form__input"
+                type="number"
+                min="1"
+                step="any"
+                autoFocus />
+            </label>
+            <label className="form__label">Currency
+              <select className="form__select" value={currencyIn}
+                onChange={({ target }) => {
+                  currencyInValue(target.value);
+                }}>
+                {currencies.map((currency) => (
+                  <option key={currency.id} value={currency.value}>
+                    {currency.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label className="form__label">Switch
+              <button
+                onClick={switchCurrencies}
+                className="form__button">
+                &#8596;</button>
+            </label>
+            <label className="form__label">Exchange
+              <select className="form__select" value={currencyOut}
+                onChange={({ target }) => {
+                  currencyOutValue(target.value);
+                }}>
+                {currencies.map((currency) => (
+                  <option key={currency.id} value={currency.value}>
+                    {currency.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+            <button className="form__button">Calculate</button>
         </fieldset>
-      </form>
+      </form >
     );
   };
 
