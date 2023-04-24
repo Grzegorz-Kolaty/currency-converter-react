25cd6cda66fd4c71252261579300358de0683215
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-24 14:35:11 +0200

    currencies moved to other file

--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ import Form from "./Form";
 function App() {
   const [result, setResult] = useState("");
 
-  const currencies = [
-    { id: 1, name: "PLN", value: 1 },
-    { id: 2, name: "EUR", value: 4.66 },
-    { id: 3, name: "USD", value: 4.20 }
-  ];
-
   return (
     <Container>
       <Header title="Currency connverter - just to make things easier!" />
-      <Form setResult={setResult}
-        currencies={currencies} />
-      <Result title="Exchange value:" result={result} />
+      <Form setResult={setResult} />
+      <Result title="Exchange value: " result={result} />
       <Footer title="Copyright 2023 YouCode Industries" />
     </Container>
   )
