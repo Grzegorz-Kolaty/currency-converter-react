9e5dcd3260e4a10c969b5263087c17d5b52c784b
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-29 12:43:55 +0200

    date.js renamed to index.js

--- src/Date/index.js
+++ src/Date/index.js
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import "./style.css";
 
-const Date = () => {
+const DateWithFormat = () => {
+    const date = new Date();
     const dateAndTimeFormatted = date.toLocaleDateString("pl-PL",
         { weekday: "long", day: "numeric", month: "long" })
         + ", "
         + date.toLocaleTimeString("pl-PL");
-    console.log(`Dzisiaj jest ${dateAndTimeFormatted}`)
+
+    return (
+        <div >
+            <h2>{dateAndTimeFormatted}</h2>
+        </div>
+    );
 };
 
-export default Date;
+export default DateWithFormat;
