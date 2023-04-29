0f370d94023c4ba7925427fdb0abfd50216dac74
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-29 12:38:48 +0200

    font weight lowered

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
 const Result = ({ title, result }) => (
-  <div className="result" >
-    <h2>{title}
-      {result.calculatedResult != null ? result.calculatedResult.toFixed(2) : ''}
-      <b>&nbsp;{result.currencyOutName}</b></h2>
+  <div className="result">
+    {title}
+    {result.calculatedResult != null ? result.calculatedResult.toFixed(2) : ''}
+    <b>&nbsp;{result.currencyOutName}</b>
   </div>
 );
 
