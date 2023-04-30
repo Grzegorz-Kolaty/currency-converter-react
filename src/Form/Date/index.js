1148df03964d1d4d803a23394c5c3bbafde01008
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-04-30 09:58:54 +0200

    date formatting moved out from presentation layer

--- src/Form/Date/index.js
+++ src/Form/Date/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./style.css";
 
-const DateComponent = ({ title }) => {
+const DateComponent = () => {
   const [date, setDate] = useState(new Date());
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -10,13 +10,20 @@ const DateComponent = ({ title }) => {
     return () => clearInterval(intervalId);
   }, []);
 
-  return (
-    <div className="date">{title}
-      {date.toLocaleDateString("pl-PL", {
+  const DateFormatted = (date) => (
+    date.toLocaleDateString("pl-PL",
+      {
         weekday: "long",
         day: "numeric",
         month: "long"
-      })}, {date.toLocaleTimeString("pl-PL")}
+      })
+    + ", "
+    + date.toLocaleTimeString("pl-PL")
+  );
+
+  return (
+    <div className="date">
+      <span>Dzisiaj jest&nbsp;{DateFormatted(date)}</span>
     </div>
   );
 };
