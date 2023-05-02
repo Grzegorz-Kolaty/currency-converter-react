945c904575079cd0036f3826825d0291f396f048
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-02 22:01:12 +0200

    styled components added to Container, css removed

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled from "styled-components";
+
+export const Main = styled.main`
     padding: 20px;
     max-width: 767px;
     min-height: 100vh;
     margin: 0 auto;
-}
+`
