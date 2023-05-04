54d6a377954e5c2f43bc963e4f967123b1938233
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-04 17:57:25 +0200

    focus and hover over Datafields, colors centralized

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -12,17 +12,22 @@ export const theme = {
     },
 
     spacing: {
-        margin: "0px",
+        margin: "10px",
         padding: "20px"
     },
 
     background: {
-        color: "#277172e1"
+        colorTransparent: "transparent",
+        colorTeal: "hsla(181, 49%, 30%, 0.882)",
+        colorTealBrighter: "hsla(181, 49%, 35%, 0.882)",
+        colorBlack: "black",
+        colorDimmed: "hsla(0, 0%, 0%, 0.585)"
     }
 };
 
 export const Formular = styled.form`
     margin-bottom: 20px;
+    background-color: ${({ theme }) => theme.background.colorTransparent};
 `;
 
 export const Fieldset = styled.fieldset`
@@ -30,26 +35,27 @@ export const Fieldset = styled.fieldset`
     font-weight: ${({ theme }) => theme.font.weight};
     border: ${({ theme }) => theme.border.style};
     border-radius: ${({ theme }) => theme.border.radius};
-    ${({ theme }) => theme.spacing};
-    background-color: #0000006b;
-    box-shadow: 0px 0px 200px 50px rgb(0, 0, 0);
+    padding: ${({ theme }) => theme.spacing.padding};
+    margin: 0px;
+    background-color: ${({ theme }) => theme.background.colorDimmed};
+    box-shadow: 0px 0px 200px 50px black;
     display: flex;
     flex-flow: wrap row;
     justify-content: center;
+    align-items: center;
 `;
 
 export const Legend = styled.legend`
     border: ${({ theme }) => theme.border.style};
     border-radius: ${({ theme }) => theme.border.radius};
-    ${({ theme }) => theme.spacing}
-    background-color: ${({ theme }) => theme.background.color};
+    padding: ${({ theme }) => theme.spacing.padding};
+    background-color: ${({ theme }) => theme.background.colorTeal};
 `;
 
 export const Label = styled.label`
     display: flex;
     flex-flow: column;
     width: 150px;
-    margin: 10px;
     flex-grow: 1;
 
     ${({ submitter }) => submitter && css`
@@ -62,23 +68,29 @@ export const Datafield = styled.div`
     font-weight: ${({ theme }) => theme.font.weight};
     border: ${({ theme }) => theme.border.style};
     border-radius: ${({ theme }) => theme.border.radius};
-    ${({ theme }) => theme.spacing};
-
+    padding: ${({ theme }) => theme.spacing.padding};
+    margin: ${({ theme }) => theme.spacing.margin};
     box-shadow: 0px 0px 20px 3px black;
-    background-color: rgba(0, 0, 0, 0.647);
+    background-color: ${({ theme }) => theme.background.colorDimmed};
     transition: 300ms;
 
-    ${({ button }) => button && css`
-    background-color: #277172e1;
-
+    &:focus {
+    outline: ${({ theme }) => theme.border.style};
+    }
 
     &:hover {
-    background-color: hsla(181, 49%, 40%, 0.882),
     cursor: pointer;
     }
 
+    ${({ button }) => button && css`
+    background-color: ${({ theme }) => theme.background.colorTeal};
+
+    &:hover {
+    background-color: ${({ theme }) => theme.background.colorTealBrighter};
+    }
+
     &:active {
-    background-color: transparent;
+    background-color: ${({ theme }) => theme.background.colorTransparent};
     }
 
     `}
