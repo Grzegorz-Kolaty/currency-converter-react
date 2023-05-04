6bfbbba9ae9c398045252c4b79014d61d47cedcd
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-04 19:05:26 +0200

    global styled-components in html and body

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+    font-size: large;
+    font-weight: 400;
+    color: white;
+    background-image: url("https://i.postimg.cc/HnTvLvXn/banknotes-4073891.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`;
+
+ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
