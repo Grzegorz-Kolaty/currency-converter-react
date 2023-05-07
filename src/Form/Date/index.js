aa4717daf4b806782916c6b766e4e64c372be458
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-07 17:09:28 +0200

    useCurrentDate implemented as a hook

--- src/Form/Date/index.js
+++ src/Form/Date/index.js
@@ -1,28 +1,21 @@
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { StyledDate } from "./styled";
 
-const DateComponent = () => {
-  const [date, setDate] = useState(new Date());
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const DateFormatted = (date) => (
-    date.toLocaleDateString("pl-PL",
-      {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-      })
+const DateFormatted = (date) => {
+  return date.toLocaleDateString("pl-PL", {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  })
     + ", "
-    + date.toLocaleTimeString("pl-PL")
-  );
+    + date.toLocaleTimeString("pl-PL");
+};
+
+const DateComponent = () => {
+  const { date } = useCurrentDate();
 
   return (
-      <StyledDate>Dzisiaj jest&nbsp;{DateFormatted(date)}</StyledDate>
+    <StyledDate>Dzisiaj jest&nbsp;{DateFormatted(date)}</StyledDate>
   );
 };
 
