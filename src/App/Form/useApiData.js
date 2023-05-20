3744d57a5e9ebfa07bf23d8f8d54d2d6ef2f163d
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-05-20 13:31:48 +0200

    try/catch + async/await removed then/catch left

--- src/App/Form/useApiData.js
+++ src/App/Form/useApiData.js
@@ -12,25 +12,26 @@ export const useApiData = () => {
   });
 
   useEffect(() => {
-    const getCurrencies = async () => {
-      try {
-        const response = await axios.get(PUBLIC_API_URL);
-        const { data } = response;
-        setCurrencies({
-          loading: "success",
-          base: data.base,
-          date: data.date,
-          rates: Object.entries(data.rates).map(([name, value]) => ({ name, value })),
+    const getCurrencies = () => {
+      axios.get(PUBLIC_API_URL)
+        .then(response => {
+          const { data } = response;
+          setCurrencies({
+            loading: "success",
+            base: data.base,
+            date: data.date,
+            rates: Object.entries(data.rates).map(([name, value]) => ({ name, value })),
+          });
+        })
+        .catch(error => {
+          setCurrencies({
+            loading: "error",
+            error: error
+          });
         });
-      } catch (error) {
-        setCurrencies({
-          loading: "error",
-          error: error
-        });
-      }
     };
 
-    setTimeout(getCurrencies, 2500);
+    setTimeout(() => getCurrencies(), 2500);
   }, []);
 
   return { currencies };
